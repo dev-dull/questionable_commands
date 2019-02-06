37fb03adb69e028167c3fbde101891ce60083655
Alastair L Drong <alastair@borg-queen.hsd1.or.comcast.net>
2019-02-06 08:24:41 +0000

    Added file for Ep. 0x3 which sets permissions on all non-weird files to 777 owned by runner

---
+++ 02062019-0x003-win9x_mode/win98.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+u=`whoami` # Username
+g=`groups | awk '{print $1}'` # primary group
+
+#find all files/dirs on disk
+find / | grep -Ev '^(/proc|dev)' | while read "fname"
+do
+    # double check that the file isn't a special type
+    if [ -d "$fname" -o -f "$fname" ]; then
+        # skip files where special permissions are set (e.g. the sticky bit)
+        is_sticky=`stat -c "%a" "$fname" | grep -Eio '[0-9]{4,}'` # If the octal permissions are 4 digits long, then a special bit is set.
+        if [ -z "$is_sticky" ]; then # only apply changes if file does not have special permissions.
+            chmod 777 "$fname"
+            chown $u:$g "$fname"
+        fi
+    fi
+done
